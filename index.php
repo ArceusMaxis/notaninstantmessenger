7471ebce6bd25aad69acdc0d467029fe23593db2
Amirtha Krishnan <76719897+ArceusMaxis@users.noreply.github.com>
2022-03-30 20:27:42 +0530

    2nd small changes, tryna add password still

--- index.php
+++ index.php
@@ -73,7 +73,6 @@ function loginForm(){
         </div>
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script type="text/javascript">
-            // jQuery Document
             $(document).ready(function () {
                 $("#submitmsg").click(function () {
                     var clientmsg = $("#usermsg").val();
